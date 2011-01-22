bb5844d674af87201f60636f4c2fb1941f363bd7
Ubuntu <ubuntu@ip-10-212-142-227.ec2.internal>
2011-01-22 23:37:22 +0000

    errors

--- app/tmp/cache/views/profile_4d33940fda220a9606000003.php
+++ app/tmp/cache/views/profile_4d33940fda220a9606000003.php
@@ -1,4 +1,4 @@
-<!--cachetime:1295675212--><?php
+<!--cachetime:1295746561--><?php
 			App::import('Controller', 'Users');
 			$controller =& new UsersController();
 				$controller->plugin = $this->plugin = '';
@@ -7,7 +7,7 @@
 				$controller->layout = $this->layout = 'default';
 				$controller->webroot = $this->webroot = '/';
 				$controller->here = $this->here = '/profile/4d33940fda220a9606000003';
-				$controller->params = $this->params = unserialize(stripslashes('a:10:{s:5:\"named\";a:0:{}s:4:\"pass\";a:1:{i:0;s:24:\"4d33940fda220a9606000003\";}s:10:\"controller\";s:5:\"users\";s:6:\"action\";s:7:\"profile\";s:6:\"plugin\";N;s:3:\"url\";a:2:{s:3:\"ext\";s:4:\"html\";s:3:\"url\";s:32:\"profile/4d33940fda220a9606000003\";}s:4:\"form\";a:0:{}s:6:\"_Token\";a:5:{s:3:\"key\";s:40:\"6ad99d7ff665235eecfefe35f89838b299734caa\";s:7:\"expires\";i:1295674012;s:18:\"allowedControllers\";a:0:{}s:14:\"allowedActions\";a:0:{}s:14:\"disabledFields\";a:1:{i:0;s:4:\"time\";}}s:6:\"isAjax\";b:0;s:6:\"models\";a:2:{i:0;s:4:\"User\";i:1;s:6:\"ApiKey\";}}'));
+				$controller->params = $this->params = unserialize(stripslashes('a:10:{s:5:\"named\";a:0:{}s:4:\"pass\";a:1:{i:0;s:24:\"4d33940fda220a9606000003\";}s:10:\"controller\";s:5:\"users\";s:6:\"action\";s:7:\"profile\";s:6:\"plugin\";N;s:3:\"url\";a:2:{s:3:\"ext\";s:4:\"html\";s:3:\"url\";s:32:\"profile/4d33940fda220a9606000003\";}s:4:\"form\";a:0:{}s:6:\"_Token\";a:5:{s:3:\"key\";s:40:\"24f08a8bde54648ad6d75fc6c797210ed767097a\";s:7:\"expires\";i:1295745361;s:18:\"allowedControllers\";a:0:{}s:14:\"allowedActions\";a:0:{}s:14:\"disabledFields\";a:1:{i:0;s:4:\"time\";}}s:6:\"isAjax\";b:0;s:6:\"models\";a:2:{i:0;s:4:\"User\";i:1;s:6:\"ApiKey\";}}'));
 				$controller->action = $this->action = unserialize('s:7:"profile";');
 				$controller->data = $this->data = unserialize(stripslashes('a:0:{}'));
 				$controller->theme = $this->theme = '';
@@ -30,30 +30,4 @@
 	<li><a href="/projects">Projects</a></li>
 	<li><a href="/timers">Timers</a></li>
 	<li><a href="/users/logout">Logout</a></li>
-</ul></nav></header><div id="container"><noscript>Homkora must have javascript enabled.</noscript><div id="content"><div class="users view">
-<h2>User</h2>
-	<div class="project-view">
-		<img align="left" style="border:1px solid #444;margin:0px 5px 0px 0px;" src="http://www.gravatar.com/avatar/e1633f5b321141db3e1ba770953ace7f?s=80&d=identicon&r=pg" />		<h2>Username</h2>
-		travisberry	</div>
-	
-	<div class="project-view">
-		<h2>API Key</h2>
-		Q954bvs2caBKJsejC17F	</div>
-
-	<div id="project-view-break"></div>
-	
-	<div class="project-view">
-		<h2>Email</h2>
-		contact@travisberry.com	</div>
-
-
-	<div class="project-view">
-		<h2>Created</h2>
-		Jan-17-2011	</div>
-	<div class="bottomLinks">
-		<br />
-		<a href="/users/userEdit/4d33940fda220a9606000003" class="button addTimerIndex"><img src="/img/icon_pencil.png" alt="Add" title="Add" />Edit Account</a>	</div>
-</div>
-
-
-<div style="clear:both;"></div></div><div style="clear:both;"></div></div><footer class="transparent_class"> <a href="http://homkora.com/about">About</a>  <a href="http://homkora.com/contact">Contact</a>  <a href="http://homkora.com/privacy">Privacy</a>  <a href="http://homkora.com/faq">FAQ</a>  <a href="http://homkora.com/help">Help</a> Created by <a href="http://www.travisberry.com">Travis Berry</a> </footer><script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" type="text/javascript"></script></body></html>
+</ul></nav></header><div id="container"><noscript>Homkora must have javascript enabled.</noscript><div id="content"><div class="users view"><h2>User<div class="project-view"><img align="left" style="border:1px solid #444;margin:0px 5px 0px 0px;" src="http://www.gravatar.com/avatar/e1633f5b321141db3e1ba770953ace7f?s=80&d=identicon&r=pg" /><h2>User Name</h2>travisberry</div><div class="project-view"><h2>API Key</h2>Q954bvs2caBKJsejC17F</div><div id="project-view-break"></div><div class="project-view"><h2>Email</h2>contact@travisberry.com</div><div class="project-view"><h2>Created</h2>Jan-17-2011</div><div class="bottomLinks"><br /><a href="/users/userEdit/4d33940fda220a9606000003" class="button addTimerIndex"><img src="/img/icon_pencil.png" alt="Add" title="Add" />Edit Account</a></div></h2></div><div style="clear:both;"></div></div><div style="clear:both;"></div></div><footer class="transparent_class"> <a href="http://homkora.com/about">About</a>  <a href="http://homkora.com/contact">Contact</a>  <a href="http://homkora.com/privacy">Privacy</a>  <a href="http://homkora.com/faq">FAQ</a>  <a href="http://homkora.com/help">Help</a> Created by <a href="http://www.travisberry.com">Travis Berry</a> </footer><script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" type="text/javascript"></script></body></html>
