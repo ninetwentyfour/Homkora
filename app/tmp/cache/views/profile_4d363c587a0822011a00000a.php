a71cfb0c8df6df82b357fb62f9fafb38ab43208b
Ubuntu <ubuntu@ip-10-212-142-227.ec2.internal>
2011-01-22 01:45:07 +0000

    added tech used to about page

--- app/tmp/cache/views/profile_4d363c587a0822011a00000a.php
+++ app/tmp/cache/views/profile_4d363c587a0822011a00000a.php
@@ -1,4 +1,4 @@
-<!--cachetime:1295667798--><?php
+<!--cachetime:1295407378--><?php
 			App::import('Controller', 'Users');
 			$controller =& new UsersController();
 				$controller->plugin = $this->plugin = '';
@@ -6,8 +6,8 @@
 				$controller->base = $this->base = '';
 				$controller->layout = $this->layout = 'default';
 				$controller->webroot = $this->webroot = '/';
-				$controller->here = $this->here = '/profile/4d33940fda220a9606000003';
-				$controller->params = $this->params = unserialize(stripslashes('a:10:{s:5:\"named\";a:0:{}s:4:\"pass\";a:1:{i:0;s:24:\"4d33940fda220a9606000003\";}s:10:\"controller\";s:5:\"users\";s:6:\"action\";s:7:\"profile\";s:6:\"plugin\";N;s:3:\"url\";a:2:{s:3:\"ext\";s:4:\"html\";s:3:\"url\";s:32:\"profile/4d33940fda220a9606000003\";}s:4:\"form\";a:0:{}s:6:\"_Token\";a:5:{s:3:\"key\";s:40:\"4daac16c76dc43936ccd06ed39922d1bec21c3a8\";s:7:\"expires\";i:1295666598;s:18:\"allowedControllers\";a:0:{}s:14:\"allowedActions\";a:0:{}s:14:\"disabledFields\";a:1:{i:0;s:4:\"time\";}}s:6:\"isAjax\";b:0;s:6:\"models\";a:2:{i:0;s:4:\"User\";i:1;s:6:\"ApiKey\";}}'));
+				$controller->here = $this->here = '/profile/4d363c587a0822011a00000a';
+				$controller->params = $this->params = unserialize(stripslashes('a:10:{s:5:\"named\";a:0:{}s:4:\"pass\";a:1:{i:0;s:24:\"4d363c587a0822011a00000a\";}s:10:\"controller\";s:5:\"users\";s:6:\"action\";s:7:\"profile\";s:6:\"plugin\";N;s:3:\"url\";a:2:{s:3:\"ext\";s:4:\"html\";s:3:\"url\";s:32:\"profile/4d363c587a0822011a00000a\";}s:4:\"form\";a:0:{}s:6:\"_Token\";a:5:{s:3:\"key\";s:40:\"c4ae970310054b1dcfbe1ec32ff20372050581d4\";s:7:\"expires\";i:1295406178;s:18:\"allowedControllers\";a:0:{}s:14:\"allowedActions\";a:0:{}s:14:\"disabledFields\";a:1:{i:0;s:4:\"time\";}}s:6:\"isAjax\";b:0;s:6:\"models\";a:2:{i:0;s:4:\"User\";i:1;s:6:\"ApiKey\";}}'));
 				$controller->action = $this->action = unserialize('s:7:"profile";');
 				$controller->data = $this->data = unserialize(stripslashes('a:0:{}'));
 				$controller->theme = $this->theme = '';
@@ -26,33 +26,33 @@
 				}
 		?><!DOCTYPE html>
 <html><head><meta charset="utf-8" /><title>Time Tracking Made Simple | Homkora</title><link rel="stylesheet" type="text/css" href="/ccss/scss-style.css" /></head><body><header><h1> <a href="http://homkora.com">Homkora</a> </h1><nav><ul>
-	<li><img align="left" style="border:1px solid #444;margin:0px 5px 0px 0px;" src="http://www.gravatar.com/avatar/e1633f5b321141db3e1ba770953ace7f?s=20&d=identicon&r=pg" /><a href="/profile/4d33940fda220a9606000003">Profile</a></li>
+	<li><img align="left" style="border:1px solid #444;margin:0px 5px 0px 0px;" src="http://www.gravatar.com/avatar/84392cae981ce2c7c68f47c49348054d?s=20&d=identicon&r=pg" /><a href="/profile/4d363c587a0822011a00000a">Profile</a></li>
 	<li><a href="/projects">Projects</a></li>
 	<li><a href="/timers">Timers</a></li>
 	<li><a href="/users/logout">Logout</a></li>
 </ul></nav></header><div id="container"><noscript>Homkora must have javascript enabled.</noscript><div id="content"><div class="users view">
 <h2>User</h2>
 	<div class="project-view">
-		<img align="left" style="border:1px solid #444;margin:0px 5px 0px 0px;" src="http://www.gravatar.com/avatar/e1633f5b321141db3e1ba770953ace7f?s=80&d=identicon&r=pg" />		<h2>Username</h2>
-		travisberry	</div>
+		<img align="left" style="border:1px solid #444;margin:0px 5px 0px 0px;" src="http://www.gravatar.com/avatar/84392cae981ce2c7c68f47c49348054d?s=80&d=identicon&r=pg" />		<h2>Username</h2>
+		test34	</div>
 	
 	<div class="project-view">
 		<h2>API Key</h2>
-		Q954bvs2caBKJsejC17F	</div>
+		56ws0pxDAgy2Ac84TXtU	</div>
 
 	<div id="project-view-break"></div>
 	
 	<div class="project-view">
 		<h2>Email</h2>
-		contact@travisberry.com	</div>
+		ninetwentyfour924@gmail.com	</div>
 
 
 	<div class="project-view">
 		<h2>Created</h2>
-		Jan-17-2011	</div>
+		Jan-19-2011	</div>
 	<div class="bottomLinks">
 		<br />
-		<a href="/users/userEdit/4d33940fda220a9606000003" class="button addTimerIndex"><img src="/img/icon_pencil.png" alt="Add" title="Add" />Edit Account</a>	</div>
+		<a href="/users/userEdit/4d363c587a0822011a00000a" class="button addTimerIndex"><img src="/img/icon_pencil.png" alt="Add" title="Add" />Edit Account</a>	</div>
 </div>
 
 
