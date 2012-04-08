db2d0f22bd13674489231d68c2391057562e976e
Travis Berry <travisberry@travisberry.com>
2012-04-08 15:44:19 -0600

    removed tmp check from mailer

--- app/vendors/swift_mailer/preferences.php
+++ app/vendors/swift_mailer/preferences.php
@@ -12,9 +12,9 @@ Swift_Preferences::getInstance()->setCharset('utf-8');
 // Without these lines the default caching mechanism is "array" but this uses
 // a lot of memory.
 // If possible, use a disk cache to enable attaching large attachments etc
-if (function_exists('sys_get_temp_dir') && is_writable(sys_get_temp_dir()))
-{
-  Swift_Preferences::getInstance()
-    -> setTempDir(sys_get_temp_dir())
-    -> setCacheType('disk');
-}
+// if (function_exists('sys_get_temp_dir') && is_writable(sys_get_temp_dir()))
+// {
+//   Swift_Preferences::getInstance()
+//     -> setTempDir(sys_get_temp_dir())
+//     -> setCacheType('disk');
+// }
